9968381be955656754a7bd013795f7e3420445ce
fedeparrilla10 <federicoivanpd@gmail.com>
2024-01-25 21:15:53 +0200

    [add] ghpages [feature] add section

---
+++ src/data/general_demo.js
@@ -0,0 +1,17 @@
+export const generalDemoData = [
+  {
+    id: 1,
+    video: '/silla675/balanceo.mp4',
+    class: 'w-16 h-16 pl-5',
+  },
+  {
+    id: 2,
+    video: '/silla675/elevador.mp4',
+    class: 'w-16 h-16',
+  },
+  {
+    id: 3,
+    video: '/silla675/reclinable.mp4',
+    class: 'w-16 h-16',
+  },
+];
